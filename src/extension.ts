7005daf31a3c40c7e2e2df0ff64fad3b2b4eacc6
RicoP <RicoP@users.noreply.github.com>
2025-10-05 11:50:04 +0200

    WIP backslashes

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+async function tidyUpBackslashes() {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showErrorMessage("No active editor found.");
+		return;
+	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ricos-c-code-utility" is now active!');
+	const document = editor.document;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ricos-c-code-utility.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Ricos C Code Utility!');
-	});
+	// Edit the first 10 lines or up to the last line if fewer
+	const edit = new vscode.WorkspaceEdit();
+	const lineCount = Math.min(10, document.lineCount);
 
-	context.subscriptions.push(disposable);
+	for (let i = 0; i < lineCount; i++) {
+		const line = document.lineAt(i);
+		let text = line.text;
+
+		// Only add backslash if not already present
+		if (!text.trimEnd().endsWith("\\")) {
+			text = text + " \\";
+		}
+
+		// Replace the entire line with the updated version
+		edit.replace(document.uri, line.range, text);
+	}
+
+	await vscode.workspace.applyEdit(edit);
+	await document.save();
+
+	vscode.window.showInformationMessage("Added backslashes to first 10 lines!");
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+	const backslashes = vscode.commands.registerCommand('ricos-c-code-utility.cbackslashes', () => {
+		tidyUpBackslashes();
+	});
+
+	context.subscriptions.push(backslashes);
+}
+
 export function deactivate() {}
